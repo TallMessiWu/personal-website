08d3059427d7d7a001f6c6ad8c29bfc2e87b42b7
Junlin Wu <tallmessiwu@qq.com>
2025-08-10 14:15:08 +0800

    :construction: wip(header): 初步实现主页顶部导航栏

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,31 @@
 <template>
-  <h1>TheHeader</h1>
+  <el-row class="container" type="flex" justify="space-between" align="middle">
+
+    <!--logo-->
+    <el-col :span="6" :offset="1">
+      <el-row>
+        <img class="logo" src="@/assets/logo.png" alt="logo"/>
+      </el-row>
+    </el-col>
+
+    <!--选项-->
+    <el-col :span="10">
+      <el-row>
+        <el-col :span="4">
+          首页
+        </el-col>
+        <el-col :span="4">
+          视频
+        </el-col>
+        <el-col :span="4">
+          音乐
+        </el-col>
+        <el-col :span="4">
+          项目
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
 </template>
 
 <script setup lang="ts">
@@ -7,5 +33,13 @@
 </script>
 
 <style scoped>
+.container {
+  padding: 1rem 0 1rem 0;
+  border-bottom: 1px solid #e5e5e5;
+}
+
+.logo {
+  width: 3rem;
+}
 
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 
 import "./main.less"
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+// 使用Element Plus UI 库
 app.use(ElementPlus)
+// 注册所有
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
 app.use(router)
 
 // 用来处理路由跳转后页面滚动到顶部
